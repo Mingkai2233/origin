c843bf4e72c8cbcf377b959eeb47b038ecfe7066
Gr4y <mingkai.zhang@stu.ahu.edu.cn>
2022-04-04 10:14:54 +0800

    modify 03 write 07 08

--- normalPractice/lanqiao/basic-03.py
+++ normalPractice/lanqiao/basic-03.py
@@ -1,8 +1,17 @@
 n, m = map(int, input().split(' '))
 chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 base = chars[:m]
-print(base)
-for row in range(n-1):
-    tmp = base[:m-1]
-    base = chars[(ord(tmp[0])-ord('A')+1)%26]+tmp
-    print(base)
+mode = 1
+tmp = 'A'
+if m == 1:
+    for i in range(n):
+        print(chr((ord(tmp)+i)))
+else:
+    for row in range(1, n+1):
+        print(base)
+        tmp = base[:m-1]
+        # if base[0] == 'A':
+        #     mode = 1
+        # if base[0] == chars[m-1]:
+        #     mode = -1
+        base = chars[(ord(tmp[0])-ord('A')+mode)%26]+tmp
